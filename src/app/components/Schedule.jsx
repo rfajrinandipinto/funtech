8e895d03b704dc9ac379924feab20f1eff216afa
R Fajri Nandipinto <nandipintorahmatfajri@gmail.com>
2022-11-27 19:14:32 +0700

    Update Schedule.jsx

--- src/app/components/Schedule.jsx
+++ src/app/components/Schedule.jsx
@@ -244,7 +244,7 @@ const Schedule = () => {
               id="demo-simple-select"
               value={hari}
               onChange={handleChangeHari}
-              inputProps={{
+              children={{
                 "data-cy": "form-day",
               }}
             >
