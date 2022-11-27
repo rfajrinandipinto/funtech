d9049e033d6d9977d534aa9406fe24491f854bfc
R Fajri Nandipinto <nandipintorahmatfajri@gmail.com>
2022-11-27 22:05:40 +0700

    fa

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Checkin />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/detail" element={<Detail />} />
+          <Route path="/detail/:day" element={<Detail />} />
         </Routes>
       </BrowserRouter>
     </div>
